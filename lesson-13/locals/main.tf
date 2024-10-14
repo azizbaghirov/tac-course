8e59dd62aa718566d4196fb560a131fabebb67e2
azizbaghirov <aziz.baghirov@gmail.com>
2024-10-14 16:06:08 +0400

    EDited main.tf for lesson 13 localS

--- lesson-13/locals/main.tf
+++ lesson-13/locals/main.tf
@@ -15,15 +15,34 @@ provider "aws" {
 
 ## Build an IAM user account resource here with a for_each meta-argument referecing a locals block.
 
+resource "aws_iam_user" "accounts_marketing" {
+  for_each = local.accounts
+  name = each.key
 
+  tags = local.common_tags
+}
 
 ## Then build a locals block with a toset function listing 4 users.
 
+locals {
+  accounts = toset (
+    ["Alice",
+    "Bob",
+    "Charlie",
+    "Denise"]
+  )
+  }
 
 
 ## Finally, build a locals block with common tags and reference it from the IAM user resource.
 
-
+locals {
+  common_tags = {
+    Company = "Widgets-R-Us"
+    Department = "Marketing"
+    Tiem_created = timestamp()
+  }
+}
 
 ## More information on Locals: https://developer.hashicorp.com/terraform/language/values/locals
 
